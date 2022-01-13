3f66c848c0adbf19fa16448dbfeb4a477b1424f2
ShuGo <schw4one1win@gmail.com>
2022-01-13 23:52:23 +0900

    Add column for Resist user page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_141949) do
+ActiveRecord::Schema.define(version: 2022_01_13_142643) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2021_12_14_141949) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "name"
+    t.integer "post_code"
+    t.string "address"
+    t.text "profile"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
