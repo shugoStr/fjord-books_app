771e111346429c4e5bcdca676c1b354145aa4375
ShuGo <schw4one1win@gmail.com>
2022-01-23 15:01:29 +0900

    Implement users show page
    Set up controller, routes, something

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "books#index"
-  get 'users/index'
-  get 'users/show'
+  resources :users, only: [:index, :show]
 end
