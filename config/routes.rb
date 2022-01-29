519cf2fe39783db22fb3bdb15338cf637985844e
ShuGo <schw4one1win@gmail.com>
2022-01-30 00:18:07 +0900

    Implement Recoverable

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "books#index"
+
   resources :users, only: [:index, :show]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
