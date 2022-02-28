f831996b853d6ee3546133306316cc59c1eb2cec
ShuGo <schw4one1win@gmail.com>
2022-03-01 00:20:42 +0900

    Implement friendships_controller

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,12 @@
+class FriendshipsController < ApplicationController
+  def create
+    user = User.find(params[:followed_id])
+    current_user.follow(params[:followed_id])
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    current_user.unfollow(params[:id])
+    redirect_to user_path(params[:id])
+  end
+end
