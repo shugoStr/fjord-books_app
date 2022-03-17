f8f3790802d39602cf31c02306454357b8dccf36
ShuGo <schw4one1win@gmail.com>
2022-03-17 22:56:32 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :active_friendships,
-           class_name: "Friendship",
-           foreign_key: "follower_id",
+           class_name: 'Friendship',
+           foreign_key: 'follower_id',
            dependent: :destroy,
            inverse_of: :follower
 
   has_many :passive_friendships,
-           class_name: "Friendship",
-           foreign_key: "followed_id",
+           class_name: 'Friendship',
+           foreign_key: 'followed_id',
            dependent: :destroy,
            inverse_of: :followed
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
-  resources :users, only: %i(index show) do
+  resources :users, only: %i[index show] do
     resources :followings, only: :index, module: :users
     resources :followers, only: :index, module: :users
   end
-  resources :friendships, only: %i(create destroy)
+  resources :friendships, only: %i[create destroy]
 end
