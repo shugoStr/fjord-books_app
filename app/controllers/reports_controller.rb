e7a512c771f7664f7b6af2dea53a176bb47e782d
ShuGo <schw4one1win@gmail.com>
2022-03-22 15:19:22 +0900

    Impelment logic to judge contributors Only the contributor can edit or delete

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,7 +22,7 @@ class ReportsController < ApplicationController
   # POST /reports
   def create
     @report = Report.new(report_params)
-    @report.user_id = current_user.id
+    @report.user = current_user
 
     respond_to do |format|
       if @report.save!
@@ -35,20 +35,24 @@ class ReportsController < ApplicationController
 
   # PATCH/PUT /reports/1
   def update
-    respond_to do |format|
-      if @report.update!(report_params)
-        format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity}
+    if current_user == @report.user
+      respond_to do |format|
+        if @report.update!(report_params)
+          format.html { redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human) }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   # DELETE /reports/1
   def destroy
-    @report.destroy!
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+    if current_user == @report.user
+      @report.destroy!
+      respond_to do |format|
+        format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+      end
     end
   end
 
