7a2d395a987eb361672c01626616552174c91433
ShuGo <schw4one1win@gmail.com>
2022-04-02 14:49:39 +0900

    Implement comment func at report view

--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,2 +1,15 @@
 class Reports::CommentsController < ApplicationController
+  def create
+    @commentable = Report.find(params[:report_id])
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save!
+    redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
