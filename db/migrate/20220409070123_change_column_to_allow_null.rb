17bb9cbdc322e9827d6b633daf81a8d2d48ede84
ShuGo <schw4one1win@gmail.com>
2022-04-24 09:41:24 +0900

    Fix null: false commentable_type, id When the account is deleted, the daily report and comments will also be deleted.

--- db/migrate/20220409070123_change_column_to_allow_null.rb
+++ db/migrate/20220409070123_change_column_to_allow_null.rb
@@ -1,7 +1,5 @@
 class ChangeColumnToAllowNull < ActiveRecord::Migration[6.1]
   def change
     change_column_null(:comments, :user_id, true)
-    change_column_null(:comments, :commentable_id, true)
-    change_column_null(:comments, :commentable_type, true)
   end
 end
