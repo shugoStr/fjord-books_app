060fb4079a53cec6cfbfe5b2ecc7111ea801f16a
ShuGo <schw4one1win@gmail.com>
2022-04-29 17:40:45 +0900

    Implement test Report model

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?" do
+      me = User.create(email: 'me@example.com', password: 'password')
+      she = User.create(email: 'she@example.com', password: 'password')
+      report = Report.create(user_id: me.id, title: 'タイトル', content: '内容')
+      her_report = Report.create(user_id: she.id, title: 'タイトル', content: '内容')
+
+      assert report.editable?(me)
+      assert_not report.editable?(she)
+      assert her_report.editable?(she)
+      assert_not her_report.editable?(me)
+  end
+
+  test "#created_on" do
+      me = User.create(email: 'me@example.com', password: 'password')
+      report = Report.create(user_id: me.id, title: 'タイトル', content: '内容')
+
+      assert_not_equal Date, report.created_at.class
+      assert_equal Date, report.created_on.class
+  end
 end
