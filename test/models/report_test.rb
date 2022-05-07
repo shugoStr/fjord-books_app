e9b1dbf428a397d4b3ff2490f362534f46fe7924
ShuGo <schw4one1win@gmail.com>
2022-05-07 15:12:54 +0900

    Add confirm value conversion of created_on method

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -21,7 +21,7 @@ class ReportTest < ActiveSupport::TestCase
 
     assert_not_equal Date, report.created_at.class
     assert_equal Date, report.created_on.class
-    assert_equal report.title, 'タイトル'
-    assert_equal report.content, '内容'
+    assert_not_equal Date.today, report.created_at
+    assert_equal Date.today, report.created_on
   end
 end
